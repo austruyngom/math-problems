aa2a6e8c61a415bd72013f86bd328f6558a5bae8
austruyngom <austruyngom@gmail.com>
2025-05-17 04:11:43 +0000

    Add 36.ts

---
+++ src/36.ts
@@ -0,0 +1,11 @@
+import sympy as sp
+
+# Define symbols
+x, y = sp.symbols('x y')
+
+# Define the equation
+equation = x**2 + 3*x*y - y**2 - 5
+
+# Solve for y in terms of x
+solution = sp.solve(equation, y)
+print(solution)
